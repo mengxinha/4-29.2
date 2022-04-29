43fdf4ed9e1e6e7a167411d77d6c3f43b701bc7b
mengxinha <10169059+mengxinha@user.noreply.gitee.com>
2022-04-29 10:13:30 +0800

    第一次

---
+++ src/Text.java
@@ -0,0 +1,13 @@
+public class Text {
+    public static void main(String[] args) {
+        int ret = func(123);
+        System.out.println(ret);
+    }
+    public static int func(int n){
+        if(n<10){
+            return n;
+        }
+        int tem = n%10+func(n/10);
+        return tem;
+    }
+}
